73e57d9c209bbf99a75c70ee6d037649cae77f2a
ha-dahyeok <jason_1114@naver.com>
2025-05-15 16:21:23 +0900

    캘린더 130, 내비게이션바 수정

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -38,7 +38,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     width: '100%',
-    paddingTop: 1,
     paddingBottom: 30,
     position: 'relative', // 필요에 따라 'absolute' 등으로 변경 가능
     // backgroundColor: '#f0f0f0', // 배경색 등 추가 스타일 가능
